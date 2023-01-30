229a6caf4e32aa9c6984eb9058ce9abc2ef081b1
simonisf <simonisf@unisystems.gr>
2023-01-30 15:36:21 +0200

    test feature-3

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -9,7 +9,7 @@ const SignIn = () => {
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google popup</button>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
     </div>
   );
 };

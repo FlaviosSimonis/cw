d546a3dafe88f7a4801cbb010afd298d1bfa181e
simonisf <simonisf@unisystems.gr>
2023-02-16 14:46:40 +0200

    finished code for rtk

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -9,17 +9,16 @@ import { store, persistor } from './store/store';
 
 import './index.scss';
 
-const rootElement = document.getElementById('root');
+const root  = ReactDOM.createRoot(document.getElementById('root'));
 
-render(
+root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </PersistGate>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>,
-  rootElement
 );

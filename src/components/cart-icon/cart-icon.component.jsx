7473321e45a3b8f54de22c894cae6be54270fce9
simonisf <simonisf@unisystems.gr>
2023-02-21 10:50:23 +0200

    redux  saga ts migration

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 
 import { selectCartCount, selectIsCartOpen } from '../../store/cart/cart.selector';
-import { setIsCartOpen } from '../../store/cart/cart.reducer';
+import { setIsCartOpen } from '../../store/cart/cart.action';
 
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';

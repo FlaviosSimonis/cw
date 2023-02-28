bd5943df4fafcef3af43cd5ba7f78edf4cc5823f
FlaviosSimonis <flavios.simonis@gmail.com>
2023-02-28 11:54:47 +0200

    pwa prep

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import App from './App';
 import { store, persistor } from './store/store';
 import { stripePromise } from './utils/stripe/stripe.utils';
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './index.scss';
 
 const root  = ReactDOM.createRoot(document.getElementById('root'));
